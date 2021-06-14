e331a1adf808ae0e30a5879057e89de396a0f1aa
akbarhps <akbarputrasrg@gmail.com>
2021-06-14 13:49:35 +0700

    Add product.model.js

---
+++ src/model/product.model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const productSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    imageUrl: [
+        {
+            type: ObjectId,
+            ref: "Image",
+        },
+    ],
+    type: {
+        type: ObjectId,
+        ref: "Type",
+    },
+});
+
+module.exports = mongoose.model("Product", productSchema);
