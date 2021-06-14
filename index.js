62ada6f35e3e495b49eecf8ac913bb34f3b4cbc0
akbarhps <akbarputrasrg@gmail.com>
2021-06-14 14:51:14 +0700

    Implement middleware error handler

--- index.js
+++ index.js
@@ -6,24 +6,23 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(cors);
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.json());
-
+const PORT = process.env.PORT || 3001;
 const mongooseOptions = {
     useUnifiedTopology: true,
     useNewUrlParser: true
 }
 
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.json());
+
 mongoose.connect(process.env.MONGO_URL, mongooseOptions)
     .then(() => console.log("Connected to database"))
     .catch(error => console.log(error.message));
 
-app.use((error, _req, res, _next) => {
-    // Middleware error catch
-    //TODO: Implement error handler
+app.use((err, _req, res, _next) => {
+    console.log(`Status: ${err.status} | Error: ${err.message}`);
+    res.status(err.status).json({message: err.message});
 });
 
 app.listen(PORT, () => console.log('Server is running on PORT ' + PORT));
