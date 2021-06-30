0d0c878cfc83cb90c9b3d47ef2340f466c3a4f6f
akbarhps <akbarputrasrg@gmail.com>
2021-06-30 21:08:53 +0700

    Add multer for image upload

---
+++ src/util/multer.js
@@ -0,0 +1,23 @@
+const multer = require("multer");
+
+const storageConfig = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "dump/images");
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${req.body.name}_${Date.now()}_${file.originalname}`);
+    },
+});
+
+function validateFile(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+        return cb(new Error('Only image files are allowed!'), false);
+    }
+    cb(null, true);
+}
+
+exports.handleImageUpload = multer({
+    storage: storageConfig,
+    limits: {fileSize: 5000000},
+    fileFilter: validateFile
+}).array("images", 12);
